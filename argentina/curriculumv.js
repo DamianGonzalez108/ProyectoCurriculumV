f882dd119d371fc7001a4b09850ce765c2f1aeda
Damian Gonzalez <damiangonzalez108@hotmail.com>
2023-02-28 22:03:47 -0300

    proyecto cv

---
+++ argentina/curriculumv.js
@@ -0,0 +1,18 @@
+
+document.getElementById("botonacercademi").addEventListener("click", function(){
+document.getElementById("aptitudes").innerHTML = "-Mi motivación principal es convertirme en un gran profesional, por eso espero que las experiencias y habilidades que les he comentado se adapten a lo que ustedes necesitan para poder continuar creciendo profesionalmente en su equipo."
+})
+
+document.getElementById("botoneducacion").addEventListener("click",function(){
+document.getElementById("aptitudes").innerHTML= "EDUCACION: Escuela Normal Abraham Lincoln, 2017. Bachiller en Economia y Gestion de las Organizaciones. Itinerario informativo de los Recursos Humanos "
+})
+
+document.getElementById("botonaptitudes").addEventListener("click", function(){
+ location.reload()
+})
+
+
+
+
+
+
